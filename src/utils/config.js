a8609c519a352dff16a6cb4c8d7e5b4ac4c5900e
wykxldz <15779715866@163.com>
2020-10-15 23:09:29 +0800

    add pictools

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,21 +2,22 @@
 
 module.exports = {
     cwd:process.cwd(),
-    server_host:"http://47.106.35.151",
+    server_host:"http://localhost",
     mysql_host: 'localhost',
     mysql_port:3306,
     mysql_username:"root",
-    mysql_password:"156321",
+    mysql_password:"123456",
     database: 'RSB',
     // login_path:"/ligin.html",
     // main_path:"/main.html",
-    tool_translate:"/translate.html",
-    tool_crypto:"/crypto.html",
-    tool_piclinks:"/piclinks.html",
+    
+    // tool_translate:"/translate.html",
+    // tool_crypto:"/crypto.html",
+    // tool_piclinks:"/piclinks.html",
     login_path:"/page/login",
     main_path:"/page/main",
-    // tool_translate:"/page/tool/translate",
-    // tool_crypto:"/page/tool/crypto",
-    // tool_piclinks:"/page/tool/piclinks",
+    tool_translate:"/page/tool/translate",
+    tool_crypto:"/page/tool/crypto",
+    tool_piclinks:"/page/tool/piclinks",
     papers_per_page:8
   }
